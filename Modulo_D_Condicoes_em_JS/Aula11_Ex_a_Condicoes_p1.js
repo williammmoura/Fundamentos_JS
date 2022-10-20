702c5e24cbba7cb86d0e32ae93eb97ae1039a773
William Moura <williamonte@gmail.com>
2022-10-20 16:37:48 -0300

    Initial commit

---
+++ Modulo_D_Condicoes_em_JS/Aula11_Ex_a_Condicoes_p1.js
@@ -0,0 +1,8 @@
+let velocidade = 70.2;
+console.log(`A velocidade do seu carro é ${velocidade}Km/h.`);
+
+if(velocidade > 60){ //Consição SIMPLES
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
+
+console.log(`Dirija sempre usando sinto de segurança!`);
---
+++ Modulo_D_Condicoes_em_JS/Aula12_Ex_hora_do_dia.js
@@ -0,0 +1,12 @@
+let horaAgora = new Date();
+let hora = horaAgora.getHours()
+console.log(`Agora é(são) exatamente ${hora} hora(s).`);
+if(hora >= 0 && hora <= 12){
+    console.log(`Bom dia!`);
+}else if(hora > 12 && hora <= 18){
+    console.log(`Boa tarde!`);
+}else if(hora > 18 && hora < 24){
+    console.log(`Boa noite!`);
+}else if(hora < 0 || hora >= 24){
+    console.log(`Hora inválida!`);
+}
