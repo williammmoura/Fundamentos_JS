33ad15b0ca74bedaf9a70ed583a4da5dc0307662
William Monte de Moura <williamonte@gmail.com>
2022-12-20 19:06:30 -0300

    up

---
+++ Modulo_F_Repeticoes_em_JS/01_Loopings.js
@@ -0,0 +1,26 @@
+let contador = 1;
+do{
+    console.log(`Passo ${contador}`);
+    contador++ //Vai ser adicionado mais uma unidade a variável contador.
+}while(contador <= 6)
+
+
+
+/*
+//Laço de repetição
+let contador = 1;
+while (contador <= 5){
+    console.log(`Passo ${contador}`);
+    contador++ //Vai ser adicionado mais uma unidade a variável contador.
+}
+*/
+
+
+
+/*console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
